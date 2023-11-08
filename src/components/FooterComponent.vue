4bf124b9dffbdb38cfab0da9b299b7546664815f
valentioaditama <valentioaditama8@gmail.com>
2023-11-09 03:05:11 +0700

    initialize

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,50 @@
+<template>
+  <main>
+    <footer class="w-full border-t bg-white pb-12">
+      <carousel :currentIndex="currentIndex" :images="images" @increment="increment" @decrement="decrement"></carousel>
+      <div class="w-full container mx-auto flex flex-col items-center">
+        <div class="flex flex-col md:flex-row text-center md:text-left md:justify-between py-6">
+          <a href="#" class="uppercase px-3">About Us</a>
+          <a href="#" class="uppercase px-3">Privacy Policy</a>
+          <a href="#" class="uppercase px-3">Terms & Conditions</a>
+          <a href="#" class="uppercase px-3">Contact Us</a>
+        </div>
+        <div class="uppercase pb-6">&copy; myblog.com</div>
+      </div>
+    </footer>
+  </main>
+</template>
+
+<script>
+import Carousel from "./carousel.vue";
+
+export default {
+  data() {
+    return {
+      currentIndex: 0,
+      images: [
+        'https://source.unsplash.com/collection/1346951/800x800?sig=1',
+        'https://source.unsplash.com/collection/1346951/800x800?sig=2',
+        'https://source.unsplash.com/collection/1346951/800x800?sig=3',
+        'https://source.unsplash.com/collection/1346951/800x800?sig=4',
+        'https://source.unsplash.com/collection/1346951/800x800?sig=5',
+        'https://source.unsplash.com/collection/1346951/800x800?sig=6',
+        'https://source.unsplash.com/collection/1346951/800x800?sig=7',
+        'https://source.unsplash.com/collection/1346951/800x800?sig=8',
+        'https://source.unsplash.com/collection/1346951/800x800?sig=9',
+      ],
+    };
+  },
+  components: {
+    Carousel,
+  },
+  methods: {
+    increment() {
+      this.currentIndex = this.currentIndex === this.images.length - 6 ? 0 : this.currentIndex + 1;
+    },
+    decrement() {
+      this.currentIndex = this.currentIndex === this.images.length - 6 ? 0 : this.currentIndex - 1;
+    },
+  },
+};
+</script>
---
+++ src/components/carousel.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="relative w-full flex items-center invisible md:visible md:pb-12">
+    <button
+        class="absolute bg-blue-800 hover:bg-blue-700 text-white text-2xl font-bold hover:shadow rounded-full w-16 h-16 ml-12"
+        @click="decrement"
+    >
+      &#8592;
+    </button>
+    <template v-for="image in visibleImages" :key="image">
+      <img class="w-1/6 hover:opacity-75" :src="image">
+    </template>
+    <button
+        class="absolute right-0 bg-blue-800 hover:bg-blue-700 text-white text-2xl font-bold hover:shadow rounded-full w-16 h-16 mr-12"
+        @click="increment"
+    >
+      &#8594;
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    currentIndex: Number,
+    images: Array,
+  },
+  data() {
+    return {
+      itemsPerPage: 6,
+    };
+  },
+  computed: {
+    visibleImages() {
+      return this.images.slice(this.currentIndex, this.currentIndex + this.itemsPerPage);
+    },
+  },
+  methods: {
+    increment() {
+      this.$emit('increment');
+    },
+    decrement() {
+      this.$emit('decrement');
+    },
+  },
+};
+</script>
